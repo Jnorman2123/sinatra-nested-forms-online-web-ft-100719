88192e02f9076bcbbeca8ba6c3e85180b38b1235
Jonathan Norman <jnorman2123@gmail.com>
2019-11-20 17:38:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,10 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-      @ships = params[:pirate][:ships]
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
       erb :'pirates/show'
     end
   end
